799efe8ad09b132bb7272d935f741923caea75c2
GabiCodings <myemail@gmail.com>
2025-04-11 23:26:46 -0400

    Fiz a funçao de pesquisa, mas tenho que dar uma lapidada e terminar o resto #fé

--- Gabi/trab.py
+++ Gabi/trab.py
@@ -14,7 +14,7 @@ livros = [
 ]
 
 
-#Controller
+#View
 
 def exibir_mensagem(mensagem):
     print(mensagem)
@@ -22,13 +22,50 @@ def exibir_mensagem(mensagem):
 def obter_entrada(mensagem):
     return input(mensagem)
 
-def cadastro_controller (livros, livro_novo):
-    obter_entrada("Qual livro você quer cadastrar? ")
-    if obter_entrada not in livros:
-        livros.append(livro_novo.upper())
-        exibir_mensagem("Cadastrado com sucesso!")
-    else:
-        exibir_mensagem("Livro já cadastrado")
+#Model Cadastro de Livros
 
-#View
-cadastro_controller(livros, livro_novo)
+def cadastro_livros(livro, lista_livros):
+    livro = livro.upper()
+    if livro not in lista_livros:
+        lista_livros.append(livro)
+        return True
+    return False
+
+#Controller Cadastro de Livros
+
+def cadastrar_livro_controller_v3():
+    livros_para_cadastrar = [ ]
+    while True:
+        livro_novo = obter_entrada("Digite o nome do livro (ou 'FINALIZAR' para encerrar): ")
+        if livro_novo == 'FINALIZAR':
+            break
+        livros_para_cadastrar.append(livro_novo)
+
+    for livro in livros_para_cadastrar:
+        if cadastro_livros(livro, livros):
+            exibir_mensagem(f"Livro '{livro}' cadastrado com sucesso!")
+        else:
+            exibir_mensagem(f"Livro '{livro}' já cadastrado")
+
+    exibir_mensagem(list(reversed(livros)))
+
+#Model Pesquisa de Livros
+def pesquisar_livro(livro):
+    livro = livro.upper()
+    if livro in livros:
+        return True
+    return False
+
+#Controll Pesquisa Livros
+
+def pesquisa_livro_controller():
+    livros = obter_entrada("Qual Livro Você quer Pesquisar Hoje? ")
+
+    if livros in livros:
+        exibir_mensagem(f"Livro {livros} Encontrado!")
+    else:
+        exibir_mensagem("Livro não encontrado!")
+
+pesquisa_livro_controller()
+
+#lapidar as coisas
